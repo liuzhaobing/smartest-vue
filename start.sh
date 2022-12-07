94c85644afd844068d60c0b7aa3ea78857485597
sevel <sevel.liu@cloudminds.com>
2022-12-07 19:59:23 +0800

    1.add(start file)

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
 #/bin/bash
+docker stop smartest-vue
+docker rm smartest-vue
+docker rmi smartest-vue:0.1.0
+docker build -t smartest-vue:0.1.0 .
+docker run -d -p 27996:27996 --name=smartest-vue smartest-vue:0.1.0
