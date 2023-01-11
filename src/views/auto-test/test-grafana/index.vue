05fa84edc2931cda8ee58b3a9df5a6f19fd153a2
sevel <sevel.liu@cloudminds.com>
2023-01-11 16:14:28 +0800

    1.add(新增grafana嵌套iframe)

---
+++ src/views/auto-test/test-grafana/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <iframe src="http://172.16.23.33:27928/d/XLUx_hM4z/apache-jmeter-dashboard-using-core-influxdbbackendlistenerclient?orgId=1&from=now-15m&to=now" frameborder="0" align="center" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskGrafanaTable"
+}
+</script>
+
+<style scoped>
+
+</style>
